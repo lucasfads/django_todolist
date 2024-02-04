785a60f91f5f1d0190fb7f7ddd6e8fe257b5d084
Lucas Fads <lucasfads@gmail.com>
2024-02-04 20:28:23 -0300

    feat: basic tasks model working

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task_text = models.CharField(max_length=200)
+    creation_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.task_text
--- todo_list/views.py
+++ todo_list/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .models import Task
+
+
 def index(request):
-    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # context = {"latest_question_list": latest_question_list}
-    context = {}
+    tasks_list = Task.objects.order_by("-creation_date")
+    context = {"tasks_list": tasks_list}
     return render(request, "todo_list/index.html", context)
